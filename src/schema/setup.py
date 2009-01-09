89eb27e501cbb18de7da64aba231c31e7fe37295
dalloliogm <dalloliogm@gmail.com>
2009-01-09 15:56:40 +0100

    added a setup.py script

---
+++ src/schema/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'hgdp h5df schema',
+      version = '0.1',
+      packages = find_packages,
+      
+      )
---
+++ src/schema/test/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Test the HGDP H5DF database 
+use nosetests for running it 
+"""
+
+
+def setup_package():
+    a = 2
+    global a
