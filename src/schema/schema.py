d9babf58ab029887187312966ff69bb329b2a39e
dalloliogm <dalloliogm@gmail.com>
2009-01-12 13:05:20 +0100

    added a _check_valid_genotype method

--- src/schema/schema.py
+++ src/schema/schema.py
@@ -24,25 +24,39 @@ class Genotype(IsDescription):
         row = self.row
         row['snp'] = str(snp)
         row['individual'] = individual
+        genotype = _check_valid_genotype
         row['genotype'] = genotype
         row.append()
-    
+
+def _check_valid_genotype(genotype):
+    """
+    check if genotype is a valid genotype 
+    (a string of 2 characters) 
+    """
+    if not isinstance(genotype, str):
+        genotype = str(genotype)
+    if len(genotype) != 2:
+        raise TypeException('genotype should be two chars long')
+    return genotype
     
 def create_debug_file():
     """
     Create test file and tables
     """
-    h5dfile = tables.openFile('../../data/test.h5', mode = 'w', title = 'Test file')
+    h5dfile = tables.openFile('../../data/test.h5', mode = 'w', 
+                              title = 'Testfile')
     group = h5dfile.createGroup('/', 'tests', 'Test table')
     
-    genotypes_table = h5dfile.createTable(group, 'genotypes', Genotype, 'Genotypes table')
-    
+    genotypes_table = h5dfile.createTable(group, 'genotypes', 
+                                          Genotype, 'Genotypes table')
+    return genotypes_table
 
 
 def test_all():
     """
     test the current module
     """
+    logging.basicConfig(level=logging.DEBUG)
     create_debug_file()
     import doctest
     doctest.testmod()
